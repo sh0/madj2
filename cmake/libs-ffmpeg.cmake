c062017530c3a81f6d3c3f87fd6030294887b934
Sh0 <sh0@yutani.ee>
2014-11-03 07:08:27 +0900

    Fixed CMake files - FFmpeg aggregation and incorrect dependenies.

--- cmake/libs-ffmpeg.cmake
+++ cmake/libs-ffmpeg.cmake
@@ -7,23 +7,6 @@
 message(STATUS "################################################")
 message(STATUS "Checking for FFmpeg")
 
-# Find libavutil
-pkg_check_modules(AVUTIL libavutil)
-info_library("libavutil" AVUTIL FATAL_ERROR)
-
-# Find libavformat
-pkg_check_modules(AVFORMAT libavformat)
-info_library("libavformat" AVFORMAT FATAL_ERROR)
-
-# Find libavcodec
-pkg_check_modules(AVCODEC libavcodec)
-info_library("libavcodec" AVCODEC FATAL_ERROR)
-
-# Find libavresample
-pkg_check_modules(AVRESAMPLE libavresample)
-info_library("libavresample" AVRESAMPLE FATAL_ERROR)
-
-# Find libswscale
-pkg_check_modules(SWSCALE libswscale)
-info_library("libswscale" SWSCALE FATAL_ERROR)
-
+# Find
+pkg_check_modules(FFMPEG libavutil libavformat libavcodec libavresample libswscale)
+info_library("FFmpeg" FFMPEG FATAL_ERROR)
